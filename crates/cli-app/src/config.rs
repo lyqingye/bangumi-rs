39b6148b2ebfb944419b213c4e19406049a1809f
lyqingye <lyqingye@users.noreply.github.com>
2025-02-24 21:38:04 +0800

    perf: atomic update config file

--- crates/cli-app/src/config.rs
+++ crates/cli-app/src/config.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use server::config::{Config, Writer};
+use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
-
 pub fn load_from_file<P: AsRef<Path>>(path: P) -> Result<(Config, Box<dyn Writer>)> {
     let config = toml::from_str::<Config>(std::fs::read_to_string(path.as_ref())?.as_str())?;
     let writer = Box::new(ConfigWriter::new(path.as_ref().to_path_buf()));
@@ -23,7 +23,13 @@ impl ConfigWriter {
 impl Writer for ConfigWriter {
     fn write(&self, config: &Config) -> Result<()> {
         let config_str = toml::to_string(config)?;
-        std::fs::write(self.path.clone(), config_str)?;
+        let tmp_path = self.path.with_extension("tmp");
+        {
+            let mut file = std::fs::File::create(&tmp_path)?;
+            file.write_all(config_str.as_bytes())?;
+            file.sync_all()?;
+        }
+        std::fs::rename(tmp_path, self.path.clone())?;
         Ok(())
     }
 }
