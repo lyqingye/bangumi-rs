27e5ce9deedfab66b28093014fc578edafe6a618
lyqingye <1316515719@qq.com>
2025-02-27 21:22:42 +0800

    feat: add subscrition setting for enforce torrent release time after episode airdate (#138)
    Co-authored-by: lyqingye <lyqingye@users.noreply.github.com>

--- crates/server/src/api.rs
+++ crates/server/src/api.rs
@@ -66,6 +66,7 @@ pub async fn calendar(
         .column(SubscriptionColumn::ResolutionFilter)
         .column(SubscriptionColumn::LanguageFilter)
         .column(SubscriptionColumn::ReleaseGroupFilter)
+        .column(SubscriptionColumn::EnforceTorrentReleaseAfterBroadcast)
         // 联表查询
         .join_rev(
             JoinType::LeftJoin,
@@ -134,6 +135,7 @@ pub async fn get_bangumi_by_id(
         .column(SubscriptionColumn::ResolutionFilter)
         .column(SubscriptionColumn::LanguageFilter)
         .column(SubscriptionColumn::ReleaseGroupFilter)
+        .column(SubscriptionColumn::EnforceTorrentReleaseAfterBroadcast)
         // 联表查询
         .join_rev(
             JoinType::LeftJoin,
@@ -265,6 +267,7 @@ pub async fn subscribe_bangumi(
                     params.release_group_filter.clone(),
                     params.collector_interval,
                     params.metadata_interval,
+                    params.enforce_torrent_release_after_broadcast,
                 )
                 .await?;
         }
