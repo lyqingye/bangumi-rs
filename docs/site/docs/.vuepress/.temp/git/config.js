13dc83f28456dd9a03d97b5a64676c96b2a7d8be
lyqingye <lyqingye@users.noreply.github.com>
2025-03-07 01:38:42 +0800

    checkpoint

--- docs/site/docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { Contributors } from "/Users/lyqingye/workspace/me/bangumi/docs/site/node_modules/@vuepress/plugin-git/lib/client/components/Contributors.js";
-import { Changelog } from "/Users/lyqingye/workspace/me/bangumi/docs/site/node_modules/@vuepress/plugin-git/lib/client/components/Changelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", Contributors);
-    app.component("GitChangelog", Changelog);
-  },
-};
